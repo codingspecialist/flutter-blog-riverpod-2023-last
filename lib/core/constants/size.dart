1799281b1377a6e61b810213b4a42b41c12f2e15
nomadhuns <kimalss22@gmail.com>
2023-06-09 00:50:57 +0900

    로그인 페이지의 Form을 위한 클래스 생성
    - custom_auth_text_form_field.dart 생성
    - size.dart 생성
    - validator_util.dart 생성
    - login_form.dart 파일 수정

---
+++ lib/core/constants/size.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const double smallGap = 5.0;
+const double mediumGap = 10.0;
+const double largeGap = 20.0;
+const double xlargeGap = 100.0;
+
+double getScreenWidth(BuildContext context) {
+  return MediaQuery.of(context).size.width;
+}
+
+double getDrawerWidth(BuildContext context) {
+  return getScreenWidth(context) * 0.6;
+}
--- lib/views/pages/auth/login_page/components/login_form.dart
+++ lib/views/pages/auth/login_page/components/login_form.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_2/core/util/validator_util.dart';
+import 'package:flutter_blog_2/views/components/custom_auth_text_form_field.dart';
 
 class LoginForm extends StatelessWidget {
-  const LoginForm({Key? key}) : super(key: key);
+  final _formKey = GlobalKey<FormState>();
+  final _username = TextEditingController();
+  final _password = TextEditingController();
+
+  LoginForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          CustomAuthTextFormField(
+            text: "Username",
+            obscureText: false,
+            funValidator: validateUsername(),
+            controller: _username,
+          ),
+          CustomAuthTextFormField(
+            text: "Password",
+            obscureText: true,
+            funValidator: validatePassword(),
+            controller: _password,
+          ),
+          TextButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+              }
+            },
+            child: Text("Login"),
+          ),
+        ],
+      ),
+    );
   }
 }
