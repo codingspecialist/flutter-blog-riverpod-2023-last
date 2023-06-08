2cd2718e0b6d1b2b4a91a7ad25ec863378da150d
nomadhuns <kimalss22@gmail.com>
2023-06-09 01:08:51 +0900

    회원가입 페이지, 로그인 페이지와 비슷하게 구현
    - join_form.dart formField 수정
    - join_body.dart 로그인 페이지 이동 버튼 추가

--- lib/views/pages/auth/join_page/components/join_form.dart
+++ lib/views/pages/auth/join_page/components/join_form.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_2/core/constants/size.dart';
+import 'package:flutter_blog_2/core/util/validator_util.dart';
+import 'package:flutter_blog_2/views/components/custom_auth_text_form_field.dart';
 
 class JoinForm extends StatelessWidget {
-  const JoinForm({Key? key}) : super(key: key);
+  final _formKey = GlobalKey<FormState>();
+  final _username = TextEditingController();
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+
+  JoinForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          CustomAuthTextFormField(
+            text: "Username",
+            obscureText: false,
+            funValidator: validateUsername(),
+            controller: _username,
+          ),
+          const SizedBox(height: mediumGap),
+          CustomAuthTextFormField(
+            text: "Email",
+            obscureText: false,
+            funValidator: validateEmail(),
+            controller: _email,
+          ),
+          const SizedBox(height: mediumGap),
+          CustomAuthTextFormField(
+            text: "Password",
+            obscureText: true,
+            funValidator: validatePassword(),
+            controller: _password,
+          ),
+          const SizedBox(height: largeGap),
+          TextButton(
+            onPressed: () {
+            },
+            child: const Text("Join"),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/views/pages/auth/join_page/join_page.dart
+++ lib/views/pages/auth/join_page/join_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_2/views/pages/auth/join_page/components/join_body.dart';
 
 class JoinPage extends StatelessWidget {
   const JoinPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: JoinBody(),
+    );
   }
 }
