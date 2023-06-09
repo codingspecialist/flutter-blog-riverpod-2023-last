e1221c54133aa84abcffa43c374574f3f2093282
nomadhuns <kimalss22@gmail.com>
2023-06-09 09:37:37 +0900

    게시물 목록 페이지 AppBar 및 Drawer 구현
    - custom_navigator.dart 생성
    - size에 Drawer를 위한 크기 메소드 생성
    - post_list_page.dart AppBar 및 Drawer, PostListBody 클래스 연결

---
+++ lib/views/components/custom_navigator.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog_2/core/constants/size.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CustomNavigation extends ConsumerWidget {
+  final scaffoldKey;
+  const CustomNavigation(this.scaffoldKey, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      width: getDrawerWidth(context),
+      height: double.infinity,
+      color: Colors.white,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextButton(
+                onPressed: () {
+                },
+                child: const Text(
+                  "글쓰기",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+              const Divider(),
+              TextButton(
+                onPressed: () {
+                },
+                child: const Text(
+                  "회원정보보기",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+              const Divider(),
+              TextButton(
+                onPressed: () {
+                },
+                child: const Text(
+                  "로그아웃",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+              const Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
