84b3de9b19d729c5c114ed047f4632549d7de5b9
nomadhuns <kimalss22@gmail.com>
2023-06-09 09:43:02 +0900

    게시물 목록 바디 및 아이템 구현
    - post_list_body.dart 및 post_list_item.dart 구현

--- lib/views/pages/post/post_list_page/components/post_list_body.dart
+++ lib/views/pages/post/post_list_page/components/post_list_body.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_2/views/pages/post/post_list_page/components/post_list_item.dart';
 
 class PostListBody extends StatelessWidget {
   const PostListBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    return ListView.separated(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return InkWell(
+          onTap: () {
+          },
+          child: PostListItem(),
+        );
+      },
+      separatorBuilder: (context, index) {
+        return const Divider();
+      },
+    );
   }
 }
--- lib/views/pages/post/post_list_page/components/post_list_item.dart
+++ lib/views/pages/post/post_list_page/components/post_list_item.dart
@@ -5,6 +5,18 @@ class PostListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListTile(
+      title: Text("제목", style: TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: Text(
+        "내용",
+        style: TextStyle(color: Colors.black45),
+        overflow: TextOverflow.ellipsis,
+        maxLines: 1,
+      ),
+      trailing: ClipRRect(
+        borderRadius: BorderRadius.circular(50), // 네모난 이미지를 동그랗게 만들기 위한 값 설정
+        child: Image.asset('assets/default_profile.png'), // 네모난 이미지
+      ),
+    );
   }
 }
