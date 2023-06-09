89adab206a4156b7b29d99bf3c324632db7d83b9
nomadhuns <kimalss22@gmail.com>
2023-06-09 10:10:29 +0900

    게시물 상세 페이지 바디 구조화

--- lib/views/pages/post/detail_page/components/post_detail_body.dart
+++ lib/views/pages/post/detail_page/components/post_detail_body.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_2/views/pages/post/detail_page/components/post_detail_buttons.dart';
+import 'package:flutter_blog_2/views/pages/post/detail_page/components/post_detail_content.dart';
+import 'package:flutter_blog_2/views/pages/post/detail_page/components/post_detail_profile.dart';
+import 'package:flutter_blog_2/views/pages/post/detail_page/components/post_detail_title.dart';
 
 class PostDetailBody extends StatelessWidget {
   const PostDetailBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: [
+          PostDetailTitle(),
+          PostDetailProfile(),
+          PostDetailButtons(),
+          PostDetailContent(),
+        ],
+      ),
+    );
   }
 }
