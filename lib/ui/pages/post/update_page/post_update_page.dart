747d608f0cf2c45595c170619a50fa2e014058ca
nomadhuns <kimalss22@gmail.com>
2023-07-20 16:41:21 +0900

    통신에 필요한 라이브러리 제거

--- lib/ui/pages/post/update_page/post_update_page.dart
+++ lib/ui/pages/post/update_page/post_update_page.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blog_2/ui/pages/post/update_page/widgets/post_update_body.dart';
-import 'package:logger/logger.dart';
 
 class PostUpdatePage extends StatelessWidget {
   const PostUpdatePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Logger().d("UpdatePage");
     return Scaffold(
       appBar: AppBar(),
       body: PostUpdateBody(),
